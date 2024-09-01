3597605de5fe62a02b4d05599ba5f90175c8deef
Rushs321 <fyggh566@tutamail.com>
2024-09-01 22:44:36 +0530

    Update proxy.js

--- src/proxy.js
+++ src/proxy.js
@@ -1,42 +1,52 @@
-const fetch = require('node-fetch');
+const request = require('request');
 const pick = require('lodash').pick;
 const shouldCompress = require('./shouldCompress');
 const redirect = require('./redirect');
 const compress = require('./compress');
+const bypass = require('./bypass');
 const copyHeaders = require('./copyHeaders');
 
-async function proxy(req, res) {
-  try {
-    const response = await fetch(req.params.url, {
+function proxy(req, res) {
+  const reqStream = request.get(
+    req.params.url,
+    {
       headers: {
         ...pick(req.headers, ['cookie', 'dnt', 'referer']),
         'user-agent': 'Bandwidth-Hero Compressor',
         'x-forwarded-for': req.headers['x-forwarded-for'] || req.ip,
         via: '1.1 bandwidth-hero'
       },
-      method: 'GET',
-      compress: true,
-      redirect: 'follow',
-    });
+      timeout: 10000,
+      maxRedirects: 5,
+      encoding: null,
+      strictSSL: false,
+      gzip: true,
+      jar: true
+    }
+  );
 
-    if (!response.ok) {
+  reqStream.on('response', (origin) => {
+    if (origin.statusCode >= 400) {
       return redirect(req, res);
     }
 
-    copyHeaders(response, res);
+    copyHeaders(origin, res);
     res.setHeader('content-encoding', 'identity');
-    req.params.originType = response.headers.get('content-type') || '';
-    req.params.originSize = parseInt(response.headers.get('content-length'), 10) || 0;
+    req.params.originType = origin.headers['content-type'] || '';
+
+    // We're not aware of the original size at this point.
+    req.params.originSize = parseInt(origin.headers['content-length'], 10) || 0;
 
     if (shouldCompress(req)) {
-      await compress(req, res, response.body);
+      compress(req, res, reqStream);
     } else {
-      res.setHeader('x-proxy-bypass', 1);
-      response.body.pipe(res);
+      bypass(req, res, reqStream);
     }
-  } catch (err) {
+  });
+
+  reqStream.on('error', (err) => {
     redirect(req, res);
-  }
+  });
 }
 
 module.exports = proxy;
