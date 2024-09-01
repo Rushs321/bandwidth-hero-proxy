16b8554c6e2c9b5539e1f58e592aa2f51604bb0a
Rushs321 <fyggh566@tutamail.com>
2024-09-01 16:56:29 +0530

    Update proxy.js

--- src/proxy.js
+++ src/proxy.js
@@ -1,45 +1,42 @@
-const request = require('request');
+const fetch = require('node-fetch');
 const pick = require('lodash').pick;
 const shouldCompress = require('./shouldCompress');
 const redirect = require('./redirect');
 const compress = require('./compress');
-const bypass = require('./bypass');
 const copyHeaders = require('./copyHeaders');
 
-function proxy(req, res) {
-  request.get(
-    req.params.url,
-    {
+async function proxy(req, res) {
+  try {
+    const response = await fetch(req.params.url, {
       headers: {
         ...pick(req.headers, ['cookie', 'dnt', 'referer']),
         'user-agent': 'Bandwidth-Hero Compressor',
         'x-forwarded-for': req.headers['x-forwarded-for'] || req.ip,
         via: '1.1 bandwidth-hero'
       },
-      timeout: 10000,
-      maxRedirects: 5,
-      encoding: null,
-      strictSSL: false,
-      gzip: true,
-      jar: true
-    },
-    (err, origin, buffer) => {
-      if (err || origin.statusCode >= 400) {
-        return redirect(req, res);
-      }
+      method: 'GET',
+      compress: true,
+      redirect: 'follow',
+    });
 
-      copyHeaders(origin, res);
-      res.setHeader('content-encoding', 'identity');
-      req.params.originType = origin.headers['content-type'] || '';
-      req.params.originSize = buffer.length;
-
-      if (shouldCompress(req)) {
-        compress(req, res, buffer);
-      } else {
-        bypass(req, res, buffer);
-      }
+    if (!response.ok) {
+      return redirect(req, res);
     }
-  );
+
+    copyHeaders(response, res);
+    res.setHeader('content-encoding', 'identity');
+    req.params.originType = response.headers.get('content-type') || '';
+    req.params.originSize = parseInt(response.headers.get('content-length'), 10) || 0;
+
+    if (shouldCompress(req)) {
+      await compress(req, res, response.body);
+    } else {
+      res.setHeader('x-proxy-bypass', 1);
+      response.body.pipe(res);
+    }
+  } catch (err) {
+    redirect(req, res);
+  }
 }
 
 module.exports = proxy;
